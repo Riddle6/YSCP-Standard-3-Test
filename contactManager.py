b941e8b65a87e6d393b508c7ed4f22fdaf60d4cf
Riddle6 <148794251+Riddle6@users.noreply.github.com>
2025-03-27 14:48:28 +0000

    Step 1, 4, and 6 working. Step 2, 3, 5 is Work in Progress

--- contactManager.py
+++ contactManager.py
@@ -17,10 +17,6 @@ def add_contact():
     print(f"\nContact for {contact_name} added successfully.\n")
     
     
-
-
-        
-
 # Function to view all contacts
 def display_contacts():
     
@@ -29,17 +25,47 @@ def display_contacts():
     else:
         print("\n--- All Contacts ---")
         for key, value in contacts.items():
-            print(f"{contacts}: Phone - {contacts[key]} ")
+            print(f"{contacts}: Phone - {contacts[key]} \n")
         
 
 # Function to search for a contact by name
+def search_contact():
+    
+    search_contacts = input("Enter the name of the contact to search for: ").strip()
 
+    if search_contacts in contacts:
+        print(f"Contacts found: {contacts} ") # W.I.P
+    
+    else:
+        print(f"Contacts for {search_contacts} not found.")
 
 # Function to remove a contact
+def remove_contact():
+
+    remove_contacts = input("Enter the name of the contact to remove: ").strip() 
+
+    if remove_contacts in contacts:
+        contacts.pop(remove_contacts)
+        print(f"Contact for {remove_contacts} removed successfully!\n")
+
+    else:
+        print(f"\nContacts for {remove_contacts} not found.\n")
 
 
 # Function to update a contact's information
+def update_contact():
 
+    update_contacts = input("Enter the name of the contact to update: ").strip()
+
+    if update_contacts in contacts:
+        new_number = input("Enter the new phone number: ").strip()
+
+        new_email = input("Enter the new email address: ").strip()
+        # W.I.P
+        
+
+    else:
+        print(f"Contacts for {update_contacts} not found.")
 
 # Main function to run the menu-driven system
 def main():
@@ -54,10 +80,22 @@ def main():
         elif option == 2:
             display_contacts()
 
+        elif option == 3:
+            search_contact()
+
+        elif option == 4:
+            remove_contact()
+
+        elif option == 5:
+            update_contact()
+
         elif option == 6:
             print("\nThank you for using Contact Manager!")
             print("Goodbye!")
             break
+
+        else:
+            print("Unforeseen Error!")
 
 
 def display_menu():
